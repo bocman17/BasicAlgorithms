68e27ff0da1cbd2f58fa41587ad301272a7decce
Tomas Hejlik <tomas.hejlik@plus4u.net>
2022-08-21 13:34:24 +0200

    WIP 130-139

--- BasicAlgorithms/BasicExercise132.cs
+++ BasicAlgorithms/BasicExercise132.cs
@@ -10,27 +10,34 @@ namespace BasicAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Test(new[] { 1, 2, 0, 3, 5, 7, 0, 9, 11 }));
+            Console.WriteLine(Test(new[] { 1, 2, 5, 3, 5, 4, 9, 11, 6 }));
             Console.WriteLine(Test(new[] { 1, 2, 3, 4, 0 }));
-            Console.WriteLine(Test(new[] { 0,0,2,0,3 }));
-            Console.WriteLine(Test(new[] { 1, 2, 5, 3, 5, 7, 5, 9, 11 }));   
+            Console.WriteLine(Test(new[] { 0, 0, 2, 0, 3 }));
+            Console.WriteLine(Test(new[] { 1, 2, 5, 3, 5, 7, 5, 9, 11 }));
             Console.ReadLine();
         }
         // Write a C# Sharp program to create new array from a given array
         // of integers shifting all even numbers before all odd numbers.
         static string Test(int[] nums)
         {
-            int indexLength = nums.Length - 1;
-            int lastNonEven = 0;
+            int i = 0, j = nums.Length - 1;
 
-            for (int i = indexLength; i >= 0; i--)
+            while (j >= i)
             {
-                if (lastNonEven == 0) lastNonEven = i;
-                if (nums[i] % 2 != 0) nums[lastNonEven--] = nums[i];
+                if (nums[i] % 2 != 0)
+                {
+                    if (nums[j] % 2 == 0)
+                    {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        i++;
+                        j--;
+                    }
+                    else j--;
+                }
+                else i++;
             }
-
-            while (lastNonEven >= 0) nums[lastNonEven--] = 0;
-
             return String.Join(" ", nums);
         }
     }
